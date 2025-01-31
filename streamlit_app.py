0ded93ed26bcfe7db92e2fa5b8807bf51d0679eb
Rupal2905 <140495819+Rupal2905@users.noreply.github.com>
2025-01-31 15:52:59 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,7 +17,8 @@ def check_continuous_respect_sma(stock_data, sma_list=[34, 50, 55, 89, 100, 144,
             candle = stock_data.iloc[i]
             current_sma = sma.iloc[i]
 
-            if candle['Open'] < current_sma and candle['High'] < current_sma and candle['Low'] < current_sma and candle['Close'] < current_sma:
+            # Check if all conditions are true (for Open, High, Low, and Close to be below SMA)
+            if ((candle[['Open', 'High', 'Low', 'Close']] < current_sma).all()):
                 respected_continuously = False
                 break  
         if respected_continuously:
